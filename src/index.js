f25a16138c6c6410db1d81ef478667426c83067f
Navin <Navi04399@gmail.com>
2024-01-15 12:26:34 +0530

    weather app with webpack

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class WeatherApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      temperature: "27 deg C",
+      humidity: "50%",
+      pressure: "1 atm"
+    }
+    this.getWeather = this.getWeather.bind(this);
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  getWeather(event) {
+    let apiUrl = "https://62698cf5-48ab-46ad-83b7-25641f747d8e-00-2guyur95g356u.sisko.replit.dev/weatherData";
+    fetch(apiUrl).then(data => data.json()).then((res) => {
+      this.setState(state => ({
+        temperature: Math.round(((res.temperature - 32) * 5) / 9) + " deg C",
+        humidity: res.humidity + "%",
+        pressure: res.pressure + " atm"
+      }));
+    }).catch(e => {
+      console.log(e.message);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="title" className="">Weather Now</div>
+        <div id="main" className="">
+          <section id="temperature">
+            <div id="label">Temperature</div>
+            <div id="value">{this.state.temperature}</div>
+          </section>
+          <section id="humidity">
+            <div id="label">Relative-Humidity</div>
+            <div id="value">{this.state.humidity}</div>
+          </section>
+          <section id="pressure">
+            <div id="label">Pressure</div>
+            <div id="value">{this.state.pressure}</div>
+          </section>
+          <button id="getWeather" onClick={this.getWeather}>Get weather</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(<WeatherApp />);
